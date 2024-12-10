d7ac0e16987178b5695431229ec981fb17e5c8de
jagree <greenljk@mail.uc.edu>
2024-12-10 05:25:39 -0500

    don't minify

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false
+  },
   plugins: [vue({
     reactivityTransform: true
   }), vueJsx()],
